793ac624010c1eb170062bd7adc445d533d65adf
Zhou Meng Meng <zhoumengmeng3@huawei.com>
2017-06-27 09:54:37 +0800

    t

--- main.go
+++ main.go
@@ -2,11 +2,10 @@ package golanglearning
 
 import (
 	"fmt"
-	l "github.com/zmmwl/golanglearning"
 )
 
 func main(){
 	fmt.Println("Hello Tyler")
 	//l.ReflectTest()
-	l.ConstTest()
+	ConstTest()
 }
